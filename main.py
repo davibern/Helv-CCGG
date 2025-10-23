f0af7c45ed47ea03ebf82fcd427a97db5dd5d418
davibern <david.bern.pal@gmail.com>
2025-10-23 16:20:11 +0200

    Correcciones menores

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pytesseract
 import os
 from pdf2image import convert_from_path
 
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'  # Ajusta esta ruta según tu instalación
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 pdf_path = 'A00 CG11-95 LVN Automoviles junio 1996.pdf'
 poppler_path = r'C:\poppler-25.07.0\Library\bin'
@@ -18,9 +18,9 @@ for i, page in enumerate(pages):
     texto = pytesseract.image_to_string(page, lang='spa')  # OCR en español
     text_completo += f'\n--- Página {i+1} ---\n{texto}'
 
-# Guardar el texto en un archivo
+# Guardar el texto en un archivo del mismo nombre que el PDF pero .txt
 output_path = os.path.splitext(pdf_path)[0] + '.txt'
 with open(output_path, 'w', encoding='utf-8') as f:
     f.write(text_completo)
 
-print('Extracción completada. Texto guardado en resultado.txt')
+print('Extracción completada. Texto guardado.')
